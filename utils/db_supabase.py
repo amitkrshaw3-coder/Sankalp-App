52405a66d8e4539b868269952a3bdce86c954243
amitkrshaw3-coder <amit.kr.shaw.3@gmail.com>
2026-08-26 19:38:34 +0530

    Update db_supabase.py

--- utils/db_supabase.py
+++ utils/db_supabase.py
@@ -1,1 +1,11 @@
+import streamlit as st
+from supabase import create_client, Client
 
+@st.cache_resource
+def init_connection() -> Client:
+    url = st.secrets["SUPABASE_URL"]
+    key = st.secrets["SUPABASE_KEY"]
+    return create_client(url, key)
+
+# Yahi woh variable hai jo missing tha!
+supabase = init_connection()
